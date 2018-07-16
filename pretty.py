36baeeaa99eb53fc55f644cc8752c2a3ea3a2df3
Alex Dzyoba <alex@dzyoba.com>
2018-07-16 21:22:54 +0300

    Add word and newline (indentation) states

--- pretty.py
+++ pretty.py
@@ -6,6 +6,9 @@ from fsm import FSM
 
 class Pretty:
     output = sys.stdout
+    indent = ""
+    indent_level = 0
+    indent_size = 4 # spaces
 
     def error(self, fsm: FSM):
         print("state: {}, symbol: '{}'".format(fsm.current_state, fsm.input_symbol))
@@ -15,17 +18,47 @@ class Pretty:
     def print(self, fsm: FSM):
         print(fsm.input_symbol, end="", sep="", file=self.output)
 
+    def print_space(self, fsm: FSM):
+        print(" ", end="", sep="", file=self.output)
+
+    def print_indent(self, fsm: FSM):
+        print(self.indent + fsm.input_symbol, end="", sep="", file=self.output)
+        print("\n", end="", sep="", file=self.output)
+        self.indent_level += 1
+        self.indent = ' ' * self.indent_level * self.indent_size
+        print(self.indent, end="", sep="", file=self.output)
+
+    def print_unindent(self, fsm: FSM):
+        print("\n", end="", sep="", file=self.output)
+        self.indent_level -= 1
+        self.indent = ' ' * self.indent_level * self.indent_size
+        print(self.indent + fsm.input_symbol, end="", sep="", file=self.output)
+
     def __init__(self):
         self.fsm = FSM("router", {})
         self.fsm.default_transition = self.error
+
+        self.fsm.add_transition_any("router", action=self.print, next_state=None)
+
         self.fsm.add_transition_list("\"'`", "router", self.print, "string")
         self.fsm.add_transition_list("\"'`", "string", self.print, "router")
         self.fsm.add_transition_any("string", action=self.print, next_state=None)
 
         self.fsm.add_transition_list(";\n", "router", self.print, "newline")
+        self.fsm.add_transition_list("{", "router", self.print_indent, "newline")
+        self.fsm.add_transition_list("}", "router", self.print_unindent, "newline")
+
         self.fsm.add_transition_list(" \t\n", "newline", None)
         self.fsm.add_transition_list("\"'`", "newline", self.print, "string")
-        self.fsm.add_transition_any("newline", action=None, next_state="router")
+        self.fsm.add_transition_any("newline", action=self.print, next_state="router")
+
+        self.fsm.add_transition_list(" \t", "router", self.print_space, "word")
+        self.fsm.add_transition_list(" \t", "word", action=None, next_state=None)
+        self.fsm.add_transition_list(" \t", "word", action=None, next_state=None)
+        self.fsm.add_transition_list("{", "word", self.print_indent, "newline")
+        self.fsm.add_transition_list("}", "word", self.print_unindent, "newline")
+        self.fsm.add_transition_any("word", action=self.print, next_state="router")
+
 
     def run(self, data):
         for c in data:
--- pretty_test.py
+++ pretty_test.py
@@ -1,6 +1,7 @@
 import io
 
-import pretty
+from pretty import Pretty
+
 
 def test_string():
     test = """
@@ -11,14 +12,36 @@ zz"
 asd
 qwe`"""
 
-    p = pretty.Pretty()
+    check_input(test, test)
 
+
+def test_word():
+    tests = [("a  b   c", "a b c"), ("a	b	c", "a b c"), ("123	45  6", "123 45 6")]
+
+    for test in tests:
+        check_input(test[0], test[1])
+
+
+def test_indent():
+    tests = [("fn some(param) { hi }", "fn some(param) {\n    hi \n}")]
+
+    for test in tests:
+        check_input(test[0], test[1])
+
+
+def check_input(inp: str, expected: str):
+    p = Pretty()
+
+    # Output pretty printed text to string instead of stdout
+    # to compare with expected result
     s = io.StringIO()
     p.output = s
 
-    p.run(test)
+    p.run(inp)
 
+    # Read pretty printed text
+    # Seek is needed here because it is a stream over string
     s.seek(0)
     output = s.read()
 
-    assert output == test
+    assert output == expected
